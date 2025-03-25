d82fb698d9008e3e32fe7e86ecb6dc5f743ddb2a
Ayesha7622 <saddiqa7676@gmail.com>
2025-03-25 19:20:04 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4651,9 +4651,13 @@ class MyApp extends StatelessWidget {
                   children: [
                     SizedBox(height: 10, width: 5),
                     CircleAvatar(
-                      radius: 25,
-                      backgroundColor: Colors.white,
-                      backgroundImage: AssetImage('assets/nature.jpg'),
+                      radius: 27,
+                      backgroundColor: Colors.green,
+                      child: CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage('assets/nature.jpg'),
+                      ),
                     ),
                     SizedBox(
                       width: 15,
