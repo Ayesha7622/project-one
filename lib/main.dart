3c378459532244bb452bd80b7defddc8c57f1b4f
Ayesha7622 <saddiqa7676@gmail.com>
2025-03-26 13:31:41 +0500

    kfds,nf

--- lib/main.dart
+++ lib/main.dart
@@ -5876,8 +5876,11 @@ class MyApp extends StatelessWidget {
                         radius: 30,
                       ),
                     ),
-                    CircleAvatar(
-                      radius: 30,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                      ),
                     ),
                     CircleAvatar(
                       radius: 30,
