f6e0fb165d6a86d5691694c723cbafdf98e97743
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-08 13:20:34 +0500

    jeedhjhdf

--- lib/main.dart
+++ lib/main.dart
@@ -438,118 +438,77 @@ class MyApp extends StatelessWidget
 
 */
 
-/*
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-
-Widget build(BuildContext context)
-{
-  return MaterialApp(
-    debugShowCheckedModeBanner:false,
-  home:Scaffold
-    (
-  body:Center
-    (
-child:Column
-  (
-children:
-[
-  SizedBox(height:100,),
-  Text('Login Your Account:',style:TextStyle
-    (
-    fontSize:28,
-    fontWeight:FontWeight.bold,
-    color:Colors.lightBlueAccent,
-
-    ),
-  ),
-  SizedBox(height:20),
-  Container(
-      height:50,
-      width:400,
-      decoration: BoxDecoration
-        (
-           color:Colors.tealAccent,
-           borderRadius:BorderRadius.circular(10),
-        ),
-      child:Center
-        (
-          child:Text('Enter Your E_mail',
-              style:TextStyle
-            (
-                color:Colors.white,
-              )
-          )
-      )
-  ),
-  SizedBox(height:20),
-  Container
-    (
-      height:50,
-      width:400,
-      decoration:BoxDecoration
-        (
-    color:Colors.tealAccent,
-    borderRadius:BorderRadius.circular(10)
-  ),
-    child:Center
-      (
-      child:Text
-        (
-        'Enter Your Password',
-        style:TextStyle
-        (
-          color:Colors.white
-        ),
-
-
-      )
-
-    )
-  ),
-      SizedBox(height:20),
-      Container
-        (
-          height:50,
-          width:400,
-          decoration:BoxDecoration
-            (
-              color:Colors.tealAccent,
-              borderRadius:BorderRadius.circular(10)
-            ),
-        child:Center
-          (
-          child:Text
-            (
-            'Login',
-            style:TextStyle
-            (
-                color:Colors.white,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+                child: Column(children: [
+          SizedBox(
+            height: 100,
+          ),
+          Text(
+            'Login Your Account:',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.lightBlueAccent,
             ),
           ),
-        )
-
-      )
-
-]
-)
-
-)
-  )
-  );
+          SizedBox(height: 20),
+          Container(
+              height: 50,
+              width: 400,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 67, 127, 113),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                  child: Text('Enter Your E_mail',
+                      style: TextStyle(
+                        color: Colors.white,
+                      )))),
+          SizedBox(height: 20),
+          Container(
+              height: 50,
+              width: 400,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 74, 139, 125),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                  child: Text(
+                'Enter Your Password',
+                style: TextStyle(color: Colors.white),
+              ))),
+          SizedBox(height: 20),
+          Container(
+              height: 50,
+              width: 400,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 43, 117, 100),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ))
+        ]))));
+  }
 }
-}
 
-*/
+
 
 /*
 
@@ -5625,6 +5584,11 @@ class MyApp extends StatelessWidget {
 
 */
 
+
+
+
+/*
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6397,3 +6361,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
